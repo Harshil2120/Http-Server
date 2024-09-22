d4c1b56ddc4041cbcbc36c6073c712ab5f4e40d7
Harshil2120 <harshil212@icloud.com>
2024-09-21 21:33:51 -0400

    Created serversocket that delivers page

--- src/main/java/com/codesplorer/httpserver/HttpServer.java
+++ src/main/java/com/codesplorer/httpserver/HttpServer.java
@@ -2,6 +2,12 @@ package com.codesplorer.httpserver;
 
 
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import com.codesplorer.httpserver.config.Configuration;
 import com.codesplorer.httpserver.config.ConfigurationManager;
 
@@ -14,6 +20,35 @@ public static void main(String[] args) {
     
     System.out.println("Using Port: "+conf.getPort());
     System.out.println("Using WebRoot: "+conf.getWebroot());
+    
+    
+    try {
+		ServerSocket serverSocket=new ServerSocket(conf.getPort());
+		Socket socket=serverSocket.accept();
+		
+		InputStream inputStream=socket.getInputStream();
+		OutputStream outputStream=socket.getOutputStream();
+		
+		String html="<html><head><title>Http Server</title></head><body><h1>Http Server in Java</h1></body></html>";
+		
+		
+		final String CRLF="\n\r";
+		
+		String response=
+				"HTTP/1.1 200 OK"+CRLF+ "Content-Length:"+html.getBytes().length+CRLF+CRLF+html+CRLF+CRLF;
+		
+		outputStream.write(response.getBytes());
+		System.out.println("Executed this line");
+		
+		inputStream.close();
+		outputStream.close();
+		socket.close();
+		serverSocket.close();
+		
+	} catch (IOException e) {
+		
+		e.printStackTrace();
+	}
 	
 }
 }
